b87907e3c3423f8455cbedf0dc09dab9bd00d0f9
laurakciic <laurakovacic@outlook.com>
2022-08-09 08:55:05 +0200

    design layout

---
+++ P2_GuessTheFlag/P2_GuessTheFlag/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  P2_GuessTheFlag
+//
+//  Created by Laura on 09.08.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var btn1: UIButton!
+    @IBOutlet var btn2: UIButton!
+    @IBOutlet var btn3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
