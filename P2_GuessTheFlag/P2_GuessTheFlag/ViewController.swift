f633a12de8133d9090c1f3e65349c95261c015ca
laurakciic <laurakovacic@outlook.com>
2022-08-09 12:38:08 +0200

    fix typo in flag array, implement random number for correctAnswer

--- P2_GuessTheFlag/P2_GuessTheFlag/ViewController.swift
+++ P2_GuessTheFlag/P2_GuessTheFlag/ViewController.swift
@@ -13,14 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet var btn2: UIButton!
     @IBOutlet var btn3: UIButton!
     
-    var countries = [String]()
-    var score     = 0
+    var countries     = [String]()
+    var score         = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         countries += ["estonia", "france", "germany", "ireland",
-                      "italy", "monacco", "nigeria", "poland",
+                      "italy", "monaco", "nigeria", "poland",
                       "russia", "spain", "uk", "us"]
         
         btn1.layer.borderWidth = 1
@@ -35,10 +36,16 @@ class ViewController: UIViewController {
     }
 
     func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         btn1.setImage(UIImage(named: countries[0]), for: .normal)
         btn2.setImage(UIImage(named: countries[1]), for: .normal)
         btn3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
+    
 
 }
 
