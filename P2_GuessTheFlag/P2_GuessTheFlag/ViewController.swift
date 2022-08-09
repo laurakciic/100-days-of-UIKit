18afb6f1bda25592226ee110ce27fa3b92391ff6
laurakciic <laurakovacic@outlook.com>
2022-08-09 10:23:07 +0200

    making the basic game work: UIButtons and CALayer

--- P2_GuessTheFlag/P2_GuessTheFlag/ViewController.swift
+++ P2_GuessTheFlag/P2_GuessTheFlag/ViewController.swift
@@ -8,15 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var btn1: UIButton!
     @IBOutlet var btn2: UIButton!
     @IBOutlet var btn3: UIButton!
     
+    var countries = [String]()
+    var score     = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland",
+                      "italy", "monacco", "nigeria", "poland",
+                      "russia", "spain", "uk", "us"]
+        
+        btn1.layer.borderWidth = 1
+        btn2.layer.borderWidth = 1
+        btn3.layer.borderWidth = 1
+        
+        btn1.layer.borderColor = UIColor.lightGray.cgColor
+        btn2.layer.borderColor = UIColor.lightGray.cgColor
+        btn3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
 
+    func askQuestion() {
+        btn1.setImage(UIImage(named: countries[0]), for: .normal)
+        btn2.setImage(UIImage(named: countries[1]), for: .normal)
+        btn3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
