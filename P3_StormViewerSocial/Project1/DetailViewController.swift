bb2f1522bd618ad00d526e27598272da4117411a
laurakciic <laurakovacic@outlook.com>
2022-08-09 21:42:28 +0200

    UIActivityViewController - share and download image

---
+++ P3_StormViewerSocial/Project1/DetailViewController.swift
@@ -0,0 +1,66 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Laura on 08.08.2022..
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!                       // ! bc when DetailViewController is being created, image view is not loaded yet
+    
+    var selectedImage:       String?
+    var selectedImageNumber: Int?
+    var totalImageCount:     Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(selectedImageNumber!) of \(totalImageCount!)"
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))       // on tap call shareTapped, shareTapped will exist in current view controller (self), #selector says compiler that a method called shareTapped will exist and should be triggered when btn is tapped
+
+        if let imageToLoad = selectedImage {                    // checks & unwraps the optional into the selected image
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    
+    @objc func shareTapped() {
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        // UIActivityViewController says what to share
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])    // creates activity view controller saying here's the thing I want to share - image, we can also provide application acitivites to show alongside system options
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem     // on ipad UI activity view controller must be shown from somewhere on the screen and that's reffered to as it's popoverPresentationController which made this thing appear on the screen, we're saying this thing was shown from a bar button item and telling it it's out right bar button item or iPad has to be shown attached to the bar button item so you can see where it came from and tap away to dismiss it
+        present(vc, animated: true) // to show finished UI acitivity view controller ready to go on the screen
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ P3_StormViewerSocial/Project1/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Laura on 08.08.2022..
+//
+
+/*
+ In this instance it’s perfectly fine to use Bundle.main.resourcePath! and try!, because if this code fails it means our app can't read its own data so something must be seriously wrong.
+ */
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var images = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm    = FileManager.default                          // will be used to look for files
+        let path  = Bundle.main.resourcePath!                    // tell me where I can find images I added to my app
+        let items = try! fm.contentsOfDirectory(atPath: path)    // will be an array of strings containing filenames, items is set to the contents of the dir at path
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                images.append(item)
+                
+            }
+        }
+        images.sort()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    // when you need to provide a row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = images[indexPath.row]                                                            // gives the text label of the cell the same text as a picture in our array
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {  // try loading "Detail" view controller and typecasting it to be DetailViewController
+            vc.selectedImage = images[indexPath.row]                                                            // sets image property to whatever is chosen in the table
+            
+            vc.selectedImageNumber = indexPath.row + 1
+            vc.totalImageCount     = images.count
+            
+            
+            navigationController?.pushViewController(vc, animated: true)                                        // shows the screen, push the detail VC onto the navigation controller 
+        }
+    }
+
+}
+
