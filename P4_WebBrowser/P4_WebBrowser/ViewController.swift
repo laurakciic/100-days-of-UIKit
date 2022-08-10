7d07f3a471570ba0b42d6b336576003a9355ce99
laurakciic <laurakovacic@outlook.com>
2022-08-10 15:04:18 +0200

    monitor page loads: UIToolbar and UIProgressView

--- P4_WebBrowser/P4_WebBrowser/ViewController.swift
+++ P4_WebBrowser/P4_WebBrowser/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
+    var progressView: UIProgressView!
     
     override func loadView() {
         webView = WKWebView()
@@ -22,6 +23,19 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)                         //
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))    //
+        
+        progressView = UIProgressView(progressViewStyle: .default)          // new UIProgressView instance with default style, .default or .bar
+        progressView.sizeToFit()                                            // as much space as need to show progress view
+        let progressButton = UIBarButtonItem(customView: progressView)      // creates new UIBarButton item using custom view intializer - wrap up progress view in a bar button item so it can go in toolbar
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        // who we want to observe - self, what property we want to observe - estimatedProgress, which value we want -new, and a context value
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
@@ -49,5 +63,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
         title = webView.title
     }
 
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
 }
 
