2e21cb5377e3ee101a511fc5cc1c1f75c12c018f
laurakciic <laurakovacic@outlook.com>
2022-08-10 14:28:51 +0200

    open websites selected by user, UIAlertController action sheets

--- P4_WebBrowser/P4_WebBrowser/ViewController.swift
+++ P4_WebBrowser/P4_WebBrowser/ViewController.swift
@@ -20,13 +20,34 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
+    @objc func openTapped() {
+        
+        let alert = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        alert.addAction(UIAlertAction(title: "hackingwithswift.comm", style: .default, handler: openPage))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        alert.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem  // attach to iPads
+        present(alert, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {                                    // selected by user
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return } // created new url combining https and title from alert
+        webView.load(URLRequest(url: url))                                    // wraps url insided URL Req and that inside webView
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
 
