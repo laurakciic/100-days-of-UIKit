1f1290750a7c644689b8a4084f5fc43958d02dea
laurakciic <laurakovacic@outlook.com>
2022-08-10 20:59:43 +0200

    set up main view controller into table view controller

---
+++ P5_AnagramWordGame/P5_AnagramWordGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P5_AnagramWordGame
+//
+//  Created by Laura on 10.08.2022..
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
