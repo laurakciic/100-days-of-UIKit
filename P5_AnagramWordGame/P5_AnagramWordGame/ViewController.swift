21b312bee652d7d49da90e14dc7c603272ec6a64
laurakciic <laurakovacic@outlook.com>
2022-08-11 07:21:25 +0200

    reading from disk (contentsOf), loading a file into string

--- P5_AnagramWordGame/P5_AnagramWordGame/ViewController.swift
+++ P5_AnagramWordGame/P5_AnagramWordGame/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var allWords  = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {                        // load into startWords
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
     }
 
 
+    func startGame() {
+        title = allWords.randomElement()                // title of VC
+        usedWords.removeAll(keepingCapacity: true)      // removes all values from usedWords array, bc it will be used to store user's answers, also making sure when new round comes up, old guesses are removed
+        tableView.reloadData()                          // reload all rows and sections - great for changing lvl's in game
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]     // table View will show all the words user found so far, so as they find words, they'll be added to usedWords and appear on tableView straight away
+        
+        return cell
+    }
 }
 
