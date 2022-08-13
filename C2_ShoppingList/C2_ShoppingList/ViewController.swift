241d3cc43243595ef9bf133bb5408182d61bf4b4
laurakciic <laurakovacic@outlook.com>
2022-08-13 13:26:45 +0200

    push new project files

---
+++ C2_ShoppingList/C2_ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C2_ShoppingList
+//
+//  Created by Laura on 13.08.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
