cedea6a41c82df1e52dab4339c26d353028c505e
laurakciic <laurakovacic@outlook.com>
2022-08-16 22:40:12 +0200

    add submit & clear buttons and their constraints

--- P8_WordGame/P8_WordGame/ViewController.swift
+++ P8_WordGame/P8_WordGame/ViewController.swift
@@ -48,6 +48,16 @@ class ViewController: UIViewController {
         currentAnswer.isUserInteractionEnabled = false                 // so they can't activate the textbox
         view.addSubview(currentAnswer)
         
+        let submit = UIButton(type: .system)
+        submit.translatesAutoresizingMaskIntoConstraints = false
+        submit.setTitle("SUBMIT", for: .normal)
+        view.addSubview(submit)
+        
+        let clear = UIButton(type: .system)
+        clear.translatesAutoresizingMaskIntoConstraints = false
+        clear.setTitle("CLEAR", for: .normal)
+        view.addSubview(clear)
+        
         NSLayoutConstraint.activate([
             scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),               // layoutMarginsGuide - scoreLabel will have distance from the right edges
             scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
@@ -63,7 +73,15 @@ class ViewController: UIViewController {
             
             currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),          // half the width of the screen
-            currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20)         // 20 points below the clues label
+            currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20),        // 20 points below the clues label
+            
+            submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+            submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+            submit.heightAnchor.constraint(equalToConstant: 44),
+            
+            clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),                 // move to the right of the center, away from the submit btn
+            clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),                              // keep them aligned
+            clear.heightAnchor.constraint(equalToConstant: 44)
             
             // more constraints will be here
         ])
