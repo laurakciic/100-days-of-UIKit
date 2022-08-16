e3a2409612881ea5ecc1a90cc2514d2ea5a11286
laurakciic <laurakovacic@outlook.com>
2022-08-16 21:36:51 +0200

    add scoreLabel

--- P8_WordGame/P8_WordGame/ViewController.swift
+++ P8_WordGame/P8_WordGame/ViewController.swift
@@ -19,7 +19,18 @@ class ViewController: UIViewController {
         view = UIView()                 // parent class of all UIKit's view types - labels, buttons, progress views...
         view.backgroundColor = .white
         
-        // more code later
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false    // bc constraints will be made programatically, by hand
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),            // layoutMarginsGuide - scoreLabel will have distance from the right edges
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+            
+            // more constraints will be here
+        ])
     }
     
     override func viewDidLoad() {
