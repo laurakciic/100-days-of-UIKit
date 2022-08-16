dc80a71a517562e0e1b0c5acd3c53ee31d62e88c
laurakciic <laurakovacic@outlook.com>
2022-08-16 18:11:56 +0200

    add another tab to UITabBarController, add error msg if parsing data fails

--- P7_WhiteHouseFeed/P7_WhiteHouseFeed/ViewController.swift
+++ P7_WhiteHouseFeed/P7_WhiteHouseFeed/ViewController.swift
@@ -14,13 +14,29 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {               // convert to url
             if let data = try? Data(contentsOf: url) {      // fetch from API
+                
                 parse(json: data)
+                return
             }
         }
+        
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a connection problem loading the feed, please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
