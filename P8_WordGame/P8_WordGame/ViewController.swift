4921c183b5934895e38e37d3da894aaa85b55079
laurakciic <laurakovacic@outlook.com>
2022-08-16 21:26:12 +0200

    create white empty 'canvas'

--- P8_WordGame/P8_WordGame/ViewController.swift
+++ P8_WordGame/P8_WordGame/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cluesLabel: UILabel!
+    var answerLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {          // main view inside here
+        view = UIView()                 // parent class of all UIKit's view types - labels, buttons, progress views...
+        view.backgroundColor = .white
+        
+        // more code later
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
