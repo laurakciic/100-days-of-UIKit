f3f788cc29c39ef53cd57735df66d9ff3d6aa40b
laurakciic <laurakovacic@outlook.com>
2022-08-17 21:38:40 +0200

    draw a thin gray line around the buttons view

--- P8_WordGame/P8_WordGame/ViewController.swift
+++ P8_WordGame/P8_WordGame/ViewController.swift
@@ -76,6 +76,8 @@ class ViewController: UIViewController {
         
         let buttonsView = UIView()
         buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        buttonsView.layer.borderWidth = 1
+        buttonsView.layer.borderColor = UIColor.lightGray.cgColor
         view.addSubview(buttonsView)
         
         NSLayoutConstraint.activate([
