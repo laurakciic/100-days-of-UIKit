fa30e06764e880715a374fe0b5bb27ee278544a2
laurakciic <laurakovacic@outlook.com>
2022-08-18 12:00:58 +0200

    add README.md, clone project7

---
+++ P9_GCD_WhiteHouse/P7_WhiteHouseFeed/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  P7_WhiteHouseFeed
+//
+//  Created by Laura on 16.08.2022..
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions         = [Petition]()
+    var filteredPetitions = [Petition]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString: String
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredit))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterPetitions))
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        if let url = URL(string: urlString) {               // convert to url
+            if let data = try? Data(contentsOf: url) {      // fetch from API
+                
+                parse(json: data)
+                return
+            }
+        }
+        
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a connection problem loading the feed, please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()                                                    // decodes JSON into objects of our choosing
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {       // asks decoder to convert its data into a single petitions object (Petitons.self)
+            petitions = jsonPetitions.results                                          // .results matches exact name in JSON
+            filteredPetitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    @objc func filterPetitions() {
+        let filterAlert = UIAlertController(title: "Search engine", message: nil, preferredStyle: .alert)
+       
+        filterAlert.addTextField()
+        filterAlert.addAction(UIAlertAction(title: "Search", style: .default) {
+            [weak self, weak filterAlert] _ in
+            guard let stringToSearch = filterAlert?.textFields?[0].text else { return }
+            self?.search(filterInput: stringToSearch)
+        })
+        
+        filterAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(filterAlert, animated: true)
+    }
+    
+    func search(filterInput: String) {
+        filteredPetitions = filteredPetitions.filter { $0.title.contains(filterInput) || $0.body.contains(filterInput) }
+        tableView.reloadData()
+    }
+    
+    @objc func showCredit() {
+        let creditAlert = UIAlertController(title: "Credits", message: "This data comes from We The People API of the Whitehouse", preferredStyle: .alert)
+        
+        creditAlert.addAction(UIAlertAction(title: "Got it", style: .default))
+        present(creditAlert, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPetitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+//        let petition = petitions[indexPath.row]
+//        cell.textLabel?.text       = petition.title
+//        cell.detailTextLabel?.text = petition.body
+        
+        let filtered = filteredPetitions[indexPath.row]
+        cell.textLabel?.text = filtered.title
+        cell.detailTextLabel?.text = filtered.body
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailVC = DetailViewController()                                         // directly, no storyboards
+        detailVC.detailItem = petitions[indexPath.row]                                // whichever user taps
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+
+}
+
---
+++ P9_GCD_WhiteHouse/README.md
@@ -0,0 +1,18 @@
+# [Project 9 - Grand Central Dispatch](https://www.hackingwithswift.com/100/39)
+
+## Topics
+
+Grand Central Dispatch, PerformSelector, quality of service queues
+
+## Intro
+
+An iPhone XS has six CPU cores inside, and each of those six things can work independently of the others. If you use just one of them – as we have been doing all this time – then your app will never come close to using the full power of the device.
+> Apple has a powerful framework called **Grand Central Dispatch** that solves this problem remarkably easy
+
+## About
+
+In this technique project we're going to return to project 7 to solve a critical problem using GCD:
+_By downloading data from the internet in viewDidLoad() our app will lock up until all the data has been transferred.
+
+We're going to solve this problem by using GCD, which will allow us to **fetch the data without locking up** the user interface.
+> even though GCD might seem easy at first, it opens up a new raft of problems, so be careful!
