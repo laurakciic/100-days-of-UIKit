abdea9a253331c6f55c2d627fdeab9c84164ee87
laurakciic <laurakovacic@outlook.com>
2022-08-19 12:11:22 +0200

    add bouncer balls, circleOfRadius

--- P11_Pachinko/P11_Pachinko/GameScene.swift
+++ P11_Pachinko/P11_Pachinko/GameScene.swift
@@ -20,16 +20,32 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
+
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }                             // attempt to read the first touch that came in
-        let locationn = touch.location(in: self)                                    // find where the touch was in the whole game scene
+        let location = touch.location(in: self)                                     // find where the touch was in the whole game scene
         
-        let box = SKSpriteNode(color: .blue, size: CGSize(width: 64, height: 64))   // create new box 64x64
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64)) // give box a physics body matching it's size
-        box.position = locationn                                                    // position it at touch location
-        addChild(box)                                                               // add box to the screen
+        let ball = SKSpriteNode(imageNamed: "ballBlue")                             // create ball
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)     // to have balls behave like balls not rectangles
+        ball.physicsBody?.restitution = 0.4                                         // bounciness, 0-not bouncy to 1-super bouncy
+        ball.position = location
+        addChild(ball)
     }
     
+    func makeBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false                                      // object will collide with other things but won't be moved, will be fixed in place
+        addChild(bouncer)
+    }
 }
