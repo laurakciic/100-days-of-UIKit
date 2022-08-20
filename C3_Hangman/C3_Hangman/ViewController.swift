17d6f2bd60e9f34ea3445dd8dd7336841711abf1
laurakciic <laurakovacic@outlook.com>
2022-08-20 16:41:43 +0200

    load words to guess from txt file

--- C3_Hangman/C3_Hangman/ViewController.swift
+++ C3_Hangman/C3_Hangman/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var wordsToGuess = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        loadWords()
     }
 
-
+    func loadWords() {
+        if let wordsURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+            if let words = try? String(contentsOf: wordsURL) {          // load into words
+                wordsToGuess = words.components(separatedBy: "\n")
+                print(wordsToGuess)
+            }
+        }
+        
+        if wordsToGuess.isEmpty {
+            let ac = UIAlertController(title: "Error loading words", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
 }
 
