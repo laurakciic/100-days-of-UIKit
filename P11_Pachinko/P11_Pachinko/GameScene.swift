ee736011cbab138fff459da908d385336ee29997
laurakciic <laurakovacic@outlook.com>
2022-08-20 10:35:24 +0200

    increment ballCount if ball lands on a green slot

--- P11_Pachinko/P11_Pachinko/GameScene.swift
+++ P11_Pachinko/P11_Pachinko/GameScene.swift
@@ -163,6 +163,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if object.name == "good" {
             destroy(ball: ball)
             score += 1
+            ballCount += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
             score -= 1
