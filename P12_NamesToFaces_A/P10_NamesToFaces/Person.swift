41dcf61d2a5e04da2cf6eacac90d583e8438b2ad
laurakciic <laurakovacic@outlook.com>
2022-08-23 11:11:45 +0200

    conform Person class to NSCoding

--- P12_NamesToFaces_A/P10_NamesToFaces/Person.swift
+++ P12_NamesToFaces_A/P10_NamesToFaces/Person.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSCoding {
 
     var name: String
     var image: String
@@ -15,6 +15,17 @@ class Person: NSObject {
     init(name: String, image: String) {
         self.name  = name
         self.image = image
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    // encoding - writing for disk
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
     }
     
 }
