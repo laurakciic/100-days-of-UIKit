a06abe983fe338be4f40c90220b26192d4a68426
laurakciic <laurakovacic@outlook.com>
2022-08-24 14:34:14 +0200

    create starting @IBAction func tapped + center tap btn & image properly

--- P15_Animation/P15_Animation/ViewController.swift
+++ P15_Animation/P15_Animation/ViewController.swift
@@ -16,11 +16,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         imageView = UIImageView(image: UIImage(named: "penguin"))
-        imageView.center = CGPoint(x: 512, y: 384)
+        imageView.center = CGPoint(x: 690, y: 380)
         view.addSubview(imageView)
     }
 
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true                                                  // hide the button as soon as it's tapped
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {                                      // read the current animation value
+            case 0:
+                break
+            default:
+                break
+            }
+        }) { finished in
+            // here comes the code to unhide tap button
+            sender.isHidden = false
+        }
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
