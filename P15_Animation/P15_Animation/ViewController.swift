9d2dbe01a2319de39222a2858a7a4a60b8371fed
laurakciic <laurakovacic@outlook.com>
2022-08-24 14:48:03 +0200

    add animations: CGAffineTransform

--- P15_Animation/P15_Animation/ViewController.swift
+++ P15_Animation/P15_Animation/ViewController.swift
@@ -23,10 +23,28 @@ class ViewController: UIViewController {
     @IBAction func tapped(_ sender: UIButton) {
         sender.isHidden = true                                                  // hide the button as soon as it's tapped
         
-        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+//        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
             switch self.currentAnimation {                                      // read the current animation value
             case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)   // twice its normal size
                 break
+            case 1:
+                self.imageView.transform = .identity                            // reset transform
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -250, y: -250)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)    // 180 degrees
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
             default:
                 break
             }
