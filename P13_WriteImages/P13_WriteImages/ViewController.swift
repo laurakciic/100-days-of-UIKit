6ea579dedf4977e56f49dcefaa5d106b5532a698
laurakciic <laurakovacic@outlook.com>
2022-08-24 12:47:21 +0200

    apply filters with Core Image framework - CIContext, CIFilter

--- P13_WriteImages/P13_WriteImages/ViewController.swift
+++ P13_WriteImages/P13_WriteImages/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Laura on 24.08.2022..
 //
 
+import CoreImage
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -14,11 +15,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var currentImage: UIImage!
     
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Instafilter"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
     
     @objc func importPicture() {
@@ -33,10 +40,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         guard let image = info[.editedImage] as? UIImage else { return }        // pluck the image
         dismiss(animated: true)                                                 // dismiss imagePickerController
         currentImage = image                                                    // assign image to currentImage property so we can modiify it with filters
-    }                                                                           // - required so we have a copy of image that was imported 
-
-    @IBAction func changeFilter(_ sender: Any) {
+                                                                                // - required so we have a copy of image that was imported
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    @IBAction func changeFilter(_ sender: UIButton) {                           // sender must be from button not any, because Ipads need to know from where the method is called
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
+        if let popoverController = ac.popoverPresentationController {
+            popoverController.sourceView = sender                       // use the sender (tapped button) as the source for popover presentation controller, so popover comes from button
+            popoverController.sourceRect = sender.bounds                // used alongside sourceView, rectangle around sourceView - width, height, x & y relative to sourceView to offset if you want to
+        }
+        
+        present(ac, animated: true)
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        guard currentImage != nil else { return }
+        guard let actionTitle = action.title else { return }
+        
+        currentFilter = CIFilter(name: actionTitle)
+            
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
     }
     
     @IBAction func save(_ sender: Any) {
@@ -44,7 +82,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
         
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+        
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)    // centering on our image directly, halfway left, top & bottom of the image
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }           
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)   // pass in the value of slider as intensity of the current filter
+        }
+        
+        // read out finished rendered image as UIImage so we can show it to our screen (core image -> core graphics (cg image) -> ui image)
+                                                                              // currentFilter.outputImage!.extent - read the whole image
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {       // create cg image from core image filter
+            let processedImage = UIImage(cgImage: cgImage)                                                                  //
+            imageView.image = processedImage
+        }
     }
 }
 
