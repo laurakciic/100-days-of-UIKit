c66b4c4ebd73a36616108c9b970dc7011aa7814e
laurakciic <laurakovacic@outlook.com>
2022-08-24 11:12:13 +0200

    import picture + add permission key in info.plist

--- P13_WriteImages/P13_WriteImages/ViewController.swift
+++ P13_WriteImages/P13_WriteImages/ViewController.swift
@@ -7,15 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // when user selects a picture
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }        // pluck the image
+        dismiss(animated: true)                                                 // dismiss imagePickerController
+        currentImage = image                                                    // assign image to currentImage property so we can modiify it with filters
+    }                                                                           // - required so we have a copy of image that was imported 
 
     @IBAction func changeFilter(_ sender: Any) {
         
