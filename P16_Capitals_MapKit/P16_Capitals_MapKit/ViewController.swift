29c53d702bf03a8cc4fe49953f07bfebc254c164
laurakciic <laurakovacic@outlook.com>
2022-08-29 00:23:00 +0200

    add right bar button for changing maps in different ways

--- P16_Capitals_MapKit/P16_Capitals_MapKit/ViewController.swift
+++ P16_Capitals_MapKit/P16_Capitals_MapKit/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Map Type", style: .plain, target: self, action: #selector(changeMapType))
+        
         let london     = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics")
         let oslo       = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago")
         let paris      = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called city of light")
@@ -26,6 +28,38 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.addAnnotations([london, oslo, paris, rome, washington])
     }
 
+    @objc func changeMapType() {
+        let ac = UIAlertController(title: "Select map type", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "Standard", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .standard
+        }))
+        ac.addAction(UIAlertAction(title: "Satellite", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .satellite
+            
+        }))
+        ac.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .hybrid
+            
+        }))
+        ac.addAction(UIAlertAction(title: "Satellite Flyover", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .satelliteFlyover
+        }))
+        ac.addAction(UIAlertAction(title: "Hybrid Flyover", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .satelliteFlyover
+        }))
+        ac.addAction(UIAlertAction(title: "Muted Standard", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .mutedStandard
+        }))
+        present(ac, animated: true)
+        
+    }
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is Capital else { return nil }
