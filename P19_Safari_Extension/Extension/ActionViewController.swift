5a2e67d5de15506c72184f60bbdc3996938ee984
laurakciic <laurakovacic@outlook.com>
2022-08-29 13:43:18 +0200

    fix keyboard - textView adjusts size when keyboard appears: NotificationCenter

--- P19_Safari_Extension/Extension/ActionViewController.swift
+++ P19_Safari_Extension/Extension/ActionViewController.swift
@@ -21,6 +21,10 @@ class ActionViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
     
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
         // extensionContext - lets us control how to interact with parent app
         // input items - array of data that parent app sends to extension
         // input item  - array of attachments - wrapped as NSItemProvider
@@ -54,5 +58,23 @@ class ActionViewController: UIViewController {
         item.attachments = [customJavaScript]
         extensionContext?.completeRequest(returningItems: [item])
     }
-
+                                            // Notification - includes name of the notif & dict with specific info called UserInfo
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }   // size of keyboard
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue                                // concrete size of keyboard
+        let keyboardViewEndFrame   = view.convert(keyboardScreenEndFrame, from: view.window)  // correct size of keyboard in rotated screen space
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero                                                       // textView takes up all available space (the amount of push to text in from its edges)
+        } else {                            // if in didChangeFrame
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)  // size of keyboard's height rotated for our window
+        }                                                                                     // compensate for the safe area existing with at home indicator on 10 class devices - 0 on Iphone SE, 8..
+        
+        script.scrollIndicatorInsets = script.contentInset   // how much margins apply to little scrollbar on the right edge of textViews when they scroll - match the size of textView
+        
+        // make textView scroll down to show whatever user tapped on
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
 }
