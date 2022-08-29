1c99c139541633412bb63b55986739ea721dc8b9
laurakciic <laurakovacic@outlook.com>
2022-08-29 11:31:47 +0200

    let user type in JavaScript in Safari Extension - editing multiline text with UITextView

--- P19_Safari_Extension/Extension/ActionViewController.swift
+++ P19_Safari_Extension/Extension/ActionViewController.swift
@@ -11,10 +11,15 @@ import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var script: UITextView!
+    
+    var pageTitle = ""
+    var pageUrl   = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
     
         // extensionContext - lets us control how to interact with parent app
         // input items - array of data that parent app sends to extension
@@ -25,18 +30,29 @@ class ActionViewController: UIViewController {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) {   // asks itemProvider to provide us with item, closure - asynchronously
                     [weak self] (dict, error) in                                            // dict - what was provided to us by iOS
                     
-                    guard let itemDictionary = dict as? NSDictionary else { return }        
+                    guard let itemDictionary   = dict as? NSDictionary else { return }
                     guard let javaScriptValues = itemDictionary [NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
-                    print(javaScriptValues)
+                    
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageUrl   = javaScriptValues["URL"]   as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
                 }
             }
         }
     }
 
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        
+        // bundling up to send back to the iOS (Safari)
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        extensionContext?.completeRequest(returningItems: [item])
     }
 
 }
