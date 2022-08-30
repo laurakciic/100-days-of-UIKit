d7b935e2cb9ba70685da082ff5e4d5ad6aa8e56d
laurakciic <laurakovacic@outlook.com>
2022-08-30 09:10:48 +0200

    create TableVC and place it inside NavController, override delegate methods

--- C5_iOS_Notes/C5_iOS_Notes/ViewController.swift
+++ C5_iOS_Notes/C5_iOS_Notes/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var notes = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note
+        
+        return cell
+    }
 }
 
