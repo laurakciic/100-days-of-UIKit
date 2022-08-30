857380b6668ed950bc646eb1571f20463a40ff71
laurakciic <laurakovacic@outlook.com>
2022-08-30 23:36:36 +0200

    add challenge solutions

--- P24_StringPlayground.playground/Contents.swift
+++ P24_StringPlayground.playground/Contents.swift
@@ -1,2 +1,23 @@
 import UIKit
 
+// challenge 1
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        guard case self.hasPrefix(prefix) = self.hasPrefix(prefix) else { return self }
+        return prefix + self
+    }
+}
+
+// challenge 2
+extension String {
+    var isNumeric: Bool {
+        return Double(self) != nil ? true : false
+    }
+}
+
+// challenge 3
+extension String {
+    var lines: [Substring] {
+        return self.split(separator: "\n")
+    }
+}
