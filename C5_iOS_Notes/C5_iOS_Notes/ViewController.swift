f263772f65811791b0aaa55a09efc08e8ee3440b
laurakciic <laurakovacic@outlook.com>
2022-08-30 08:51:55 +0200

    push project files for C5 - recreating iOS Notes app

---
+++ C5_iOS_Notes/C5_iOS_Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C5_iOS_Notes
+//
+//  Created by Laura on 30.08.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
