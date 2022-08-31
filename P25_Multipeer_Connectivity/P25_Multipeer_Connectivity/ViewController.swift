acf984538e4a58567012b40a39f50e98fc8a76da
laurakciic <laurakovacic@outlook.com>
2022-08-31 10:51:55 +0200

    going peer to peer: MCSession, MCBrowserViewController, showConnectionPrompt()

--- P25_Multipeer_Connectivity/P25_Multipeer_Connectivity/ViewController.swift
+++ P25_Multipeer_Connectivity/P25_Multipeer_Connectivity/ViewController.swift
@@ -5,17 +5,38 @@
 //  Created by Laura on 31.08.2022..
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
 
     var images = [UIImage]()
+    
+    var peerID = MCPeerID(displayName: UIDevice.current.name)           // how peer (person) is shown on other devices - get the name of current device
+    var mcSession: MCSession?
+    var mcAdvertiserAssistant: MCAdvertiserAssistant?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Selfie Share"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession?.delegate = self      // tell us when something happened
+    }
+    
+    func startHosting(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "laura-project25", discoveryInfo: nil, session: mcSession)
+    }
+    
+    func joinSession(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        let mcBrowser = MCBrowserViewController(serviceType: "laura-project25", session: mcSession)
+        mcBrowser.delegate = self       // vc is delegate of mcSession and mcBrowser
+        present(mcBrowser, animated: true)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -44,7 +65,17 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         dismiss(animated: true)                                                 // dismiss vc
         
         images.insert(image, at: 0)                                             // insert image in array
-        collectionView.reloadData()                                             // show it 
+        collectionView.reloadData()                                             // show it
     }
+    
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: UIAlertAction.Style, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    
 }
 
