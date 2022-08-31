0c0014df92bf114ab3d9f0144e257b331775626d
laurakciic <laurakovacic@outlook.com>
2022-08-31 10:14:24 +0200

    push project files for P25 - Multipeer Connectivity project

---
+++ P25_Multipeer_Connectivity/P25_Multipeer_Connectivity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P25_Multipeer_Connectivity
+//
+//  Created by Laura on 31.08.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
