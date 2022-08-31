edda5b25550c1b91c1e115b3d057b986bc435369
laurakciic <laurakovacic@outlook.com>
2022-08-31 14:17:20 +0200

    create full-screen imageView, redraw btn & basic methods

--- P27_CoreGraphics/P27_CoreGraphics/ViewController.swift
+++ P27_CoreGraphics/P27_CoreGraphics/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        drawRectange()
     }
 
-
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectange()
+            
+        default:
+            break
+        }
+    }
+    
+    func drawRectange() {
+        
+    }
 }
 
