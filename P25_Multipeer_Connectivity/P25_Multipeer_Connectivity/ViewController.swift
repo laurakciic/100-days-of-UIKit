333ae742c269db5ce5c5163332323adf4bee5459
laurakciic <laurakovacic@outlook.com>
2022-08-31 10:33:21 +0200

    import photos in CollectionView

--- P25_Multipeer_Connectivity/P25_Multipeer_Connectivity/ViewController.swift
+++ P25_Multipeer_Connectivity/P25_Multipeer_Connectivity/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var images = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]                    // load image from array and put it in imageView image
+        }
+        
+        return cell
     }
 
-
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }        // if we can't find image in dict
+        dismiss(animated: true)                                                 // dismiss vc
+        
+        images.insert(image, at: 0)                                             // insert image in array
+        collectionView.reloadData()                                             // show it 
+    }
 }
 
