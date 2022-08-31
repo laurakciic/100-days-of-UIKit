eca3911c2b3cdd5232693c465b75476b4fda94cd
laurakciic <laurakovacic@outlook.com>
2022-08-31 14:29:56 +0200

    drawing a Core Graphics context with UIGraphicsImageRenderer

--- P27_CoreGraphics/P27_CoreGraphics/ViewController.swift
+++ P27_CoreGraphics/P27_CoreGraphics/ViewController.swift
@@ -35,7 +35,21 @@ class ViewController: UIViewController {
     }
     
     func drawRectange() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))      // canvas 512x512 points, also stores info how we want do draw
         
+        let image = renderer.image { ctx in                                                // context parameter
+            // drawing code
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)                                                 // 10 point border around rectangle, centered on the edge of rect, 5 points inside, 5 points outside 
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)                                     // fill rectangle and draw border
+        }
+        
+        imageView.image = image                                                            // put rendered image in imageView on UIView
     }
 }
 
