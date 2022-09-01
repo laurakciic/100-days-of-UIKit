6e27bda77e3e30b87e96c8397408a986a5c92406
laurakciic <laurakovacic@outlook.com>
2022-09-01 18:22:41 +0200

    push project files for C6 - Meme Maker

---
+++ C6_Meme_Generation/C6_Meme_Generation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C6_Meme_Generation
+//
+//  Created by Laura on 01.09.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
