a98f630b849432265b0ea506b4f704883e76fa31
laurakciic <laurakovacic@outlook.com>
2022-09-04 10:03:54 +0200

    comment code

--- P28_Security/P28_Security/ViewController.swift
+++ P28_Security/P28_Security/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)    // call saveSecretMessage when user leaves the app
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
@@ -50,26 +50,26 @@ class ViewController: UIViewController {
         secretTextView.isHidden = false
         title = "Secret stuff"
         
-        secretTextView.text = KeychainWrapper.standard.string(forKey: "SecretMsg") ?? ""    // try loading the key from keychain
+        secretTextView.text = KeychainWrapper.standard.string(forKey: "SecretMsg") ?? ""            // try loading the key from keychain
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveSecretMessage))
     }
     
     @objc func saveSecretMessage() {
-        guard secretTextView.isHidden == false else { return }                              // making sure secret is visible before running rest of the code
+        guard secretTextView.isHidden == false else { return }                                      // making sure secret is visible before running rest of the code
         navigationItem.rightBarButtonItem = nil
         
         KeychainWrapper.standard.set(secretTextView.text, forKey: "SecretMsg")
-        secretTextView.resignFirstResponder()                                               // make text view stop being active on the screen rn
-        secretTextView.isHidden = true                                                      // will show authenticate button behind it
+        secretTextView.resignFirstResponder()                                                       // make text view stop being active on the screen rn
+        secretTextView.isHidden = true                                                              // will show authenticate button behind it
         title = "Nothing to see here"
     }
 
     @IBAction func authenticateTapped(_ sender: Any) {
-        let context = LAContext()                                                           // LocalAuthentication context
-        var error: NSError?
+        let context = LAContext()                                                                   // LocalAuthentication context
+        var error: NSError?                                                                         // objc form of Swift error type
         
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {     // don't pass in error itself, but where it is in RAM, pointer
             let reason = "Identify yourself"
             
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
